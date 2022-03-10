05f14b6e81b27102c3d5e1b37d3e54a0288efeb4
ilias <ilbak@polymtl.ca>
2022-03-10 15:24:20 -0500

    controle de timer0

---
+++ tp/tp7/lib/timer0.cpp
@@ -0,0 +1,37 @@
+#include "timer0.h"
+
+
+Timer0(uint8_t mode, uint16_t duree){
+
+    cli();
+
+    TCNT0 = 0;
+
+    OCR0A = duree;
+
+    if(mode == 0){
+        TCCR0A |= (1 << WGM00); 
+        TCCR0B |= (1 << WGM02) | (1 << CS12) | (1 << CS10);
+        TCCR0C = 0;
+    }
+
+    TIMSK0 |= (1 << OCIE0A);
+
+    sei();
+
+}
+
+arreter(){
+
+    cli();
+
+    TCNT0 = 0;
+    OCR0A = 0;
+    TCCR0B = 0;
+    TCCR0C = 0;
+
+    TIMSK0 = 0;
+
+    sei();
+
+}
