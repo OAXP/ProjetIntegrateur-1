da0606e57f2c29cf79d7a229f4cad9ee481a9cf4
Andy Tran <andy.tran@polymtl.ca>
2022-03-11 12:28:05 -0500

    debug modification

--- tp/tp7/lib/debug.cpp
+++ tp/tp7/lib/debug.cpp
@@ -1,8 +1,20 @@
 #include "debug.h"
 #include "rs232.h"
 
+#define DEBUG // à mettre en commentaire ou non au besoin
+
+#ifdef DEBUG
+
+# define DEBUG_PRINT(donnee) {
 rs232 printer;
 
-void printer::print(donnee){
+void printer::DEBUG_PRINT(donnee){
     printer.transmissionUART(donnee)
 }
+}
+
+#else
+
+# define DEBUG_PRINT(donnee) do {} while (0) // code mort
+
+#endif
