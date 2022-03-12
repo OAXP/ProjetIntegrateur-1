96c02f5a7ea7c7320f79045e432ea64e3da08851
Andy Tran <andy.tran@polymtl.ca>
2022-03-12 10:45:52 -0500

    debug modification

--- tp/tp7/lib/debug.cpp
+++ tp/tp7/lib/debug.cpp
@@ -1,20 +1,6 @@
 #include "debug.h"
 #include "rs232.h"
 
-#define DEBUG // à mettre en commentaire ou non au besoin
-
-#ifdef DEBUG
-
-# define DEBUG_PRINT(donnee) {
-Rs232 printer;
-
-void printer::DEBUG_PRINT(donnee){
-    printer.transmissionUART(donnee)
+void DEBUG_PRINT(donnee){
+    Rs232.transmissionUART(donnee)
 }
-}
-
-#else
-
-# define DEBUG_PRINT(donnee) do {} while (0) // code mort
-
-#endif
--- tp/tp7/lib/debug.h
+++ tp/tp7/lib/debug.h
@@ -1,8 +1,9 @@
 #include <avr/io.h>
+#include "rs232.h"
 
-class debug {
-    public:
-        void DEBUG_PRINT(donnee);
-    private:
-        uint8_t donnee;
-};
+#define DEBUG // à mettre en commentaire ou non au besoin
+#ifdef DEBUG
+# define DEBUG_PRINT(donnee) Rs232.transmissionUART(donnee);
+#else
+# define DEBUG_PRINT(donnee) do {} while (0) // code mort
+#endif
