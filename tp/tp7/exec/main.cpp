94a5c5cf8db2c04d8e92c25cd41fbc02cfd7351a
Anas Barbouch <anas.barbouch@polymtl.ca>
2022-03-13 15:49:34 -0400

    Modification mineure du main

--- tp/tp7/exec/main.cpp
+++ tp/tp7/exec/main.cpp
@@ -12,37 +12,36 @@ int main(){
     Del del(&PORTA, PA0, PA1);
 
 
+    
+    moteur.avancer();
     {
-        moteur.avancer();
-        {
-            char tab[] = "Robot Avance\n";
-            uint8_t taille = sizeof(tab)/sizeof(tab[0]);
-            DEBUG_PRINT(tab, taille);
-        }
-        _delay_ms(1000);
-        moteur.reculer();
-        {
-            char tab[] = "Robot Recule\n";
-            uint8_t taille = sizeof(tab)/sizeof(tab[0]);
-            DEBUG_PRINT(tab, taille);
-        }
-        _delay_ms(1000);
-        moteur.tournerDroite();
-        {
-            char tab[] = "Robot tourne droite\n";
-            uint8_t taille = sizeof(tab)/sizeof(tab[0]);
-            DEBUG_PRINT(tab, taille);
-        }
-        _delay_ms(1000);
-        moteur.tournerGauche();
-        {
-            char tab[] = "Robot tourne gauche\n";
-            uint8_t taille = sizeof(tab)/sizeof(tab[0]);
-            DEBUG_PRINT(tab, taille);
-        }
-        _delay_ms(1000);
-        moteur.arreter();
+        char tab[] = "Robot Avance\n";
+        uint8_t taille = sizeof(tab)/sizeof(tab[0]);
+        DEBUG_PRINT(tab, taille);
     }
+    _delay_ms(1000);
+    moteur.reculer();
+    {
+        char tab[] = "Robot Recule\n";
+        uint8_t taille = sizeof(tab)/sizeof(tab[0]);
+        DEBUG_PRINT(tab, taille);
+    }
+    _delay_ms(1000);
+    moteur.tournerDroite();
+    {
+        char tab[] = "Robot tourne droite\n";
+        uint8_t taille = sizeof(tab)/sizeof(tab[0]);
+        DEBUG_PRINT(tab, taille);
+    }
+    _delay_ms(1000);
+    moteur.tournerGauche();
+    {
+        char tab[] = "Robot tourne gauche\n";
+        uint8_t taille = sizeof(tab)/sizeof(tab[0]);
+        DEBUG_PRINT(tab, taille);
+    }
+    _delay_ms(1000);
+    moteur.arreter();
     while (true)
     {
         switch (bouton.getEtat())
