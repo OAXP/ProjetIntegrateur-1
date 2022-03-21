4a79a773d8003d249b221250b11a60af2efdc242
Andy Tran <andy.tran@polymtl.ca>
2022-03-21 12:06:43 -0400

    bytecode to memory

--- tp/tp9/exec/main.cpp
+++ tp/tp9/exec/main.cpp
@@ -7,6 +7,16 @@
 
 int main(){
     Midi sonnerie;
+
+    //ecrire bytecode a la memoire
+    Rs232 writer;
+    uint8_t addresse = 0x00;
+    for (uint8_t i = 0; i < binSize; i++)
+    {
+        memoire.ecriture(adresse, writer.receiveUART());
+        addresse += 0x08;
+    }
+    
     while (true)
     { // joue la mélodie suivante : https://www.youtube.com/watch?v=0VZghMkvN6c
         sonnerie.jouerNote(64);
