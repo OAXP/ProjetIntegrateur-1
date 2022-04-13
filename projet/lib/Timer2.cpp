99bdc076362f2811584035fe955327542dbc4fd9
Andy Tran <andy.tran@polymtl.ca>
2022-04-13 16:21:46 -0400

    modif timer 2

--- projet/lib/Timer2.cpp
+++ projet/lib/Timer2.cpp
@@ -1,8 +1,8 @@
 
 /**
- * @file Timer0.cpp
+ * @file Timer2.cpp
  * @author Ilias Bakhbukh (ilias.bakhbukh@polymtl.ca)
- * @brief Classe Timer0 permet de contrôler le timer 0,
+ * @brief Classe Timer2 permet de contrôler le timer 2,
  * elle donne accès aux modes Fast PWM et CTC
  * @date 2022-03-13
  */
@@ -37,7 +37,7 @@ void Timer2::initialiser(uint8_t modeDeFonctionnement, uint16_t duree){
 
     TCCR2A |= (1 << COM2A1)| (1 << COM2B1);
 
-    TCCR2B |= (1 << CS20) | (1 << CS21); // 64 prescaler
+    TCCR2B |= (1 << CS20) | (1 << CS21) | (1 << CS22); // 1024 prescaler
 
     TIMSK2 |= (1 << OCIE2A) | (1 << OCIE2B);
 
--- projet/lib/Timer2.h
+++ projet/lib/Timer2.h
@@ -1,8 +1,8 @@
 
 /**
- * @file Timer1.h
+ * @file Timer2.h
  * @author Ilias Bakhbukh (ilias.bakhbukh@polymtl.ca)
- * @brief Classe Timer1 permet de contrôler le timer 1,
+ * @brief Classe Timer2 permet de contrôler le timer 2,
  * elle donne accès aux modes Fast PWM et CTC
  * @date 2022-03-13
  * 
